4ee30f4d7d94a115dfce3ea31d830e62ccf0bff2
dakhdjwka <59766958+PCG0@users.noreply.github.com>
2022-06-17 18:17:55 +0800

    存档

--- Lua/NewLuaFile.lua
+++ Lua/NewLuaFile.lua
@@ -1,33 +1,11 @@
 #!/usr/local/bin/lua
 
 
-require "socket"
-function sleep(sec)
-    socket.select(nil, nil, sec)
-end
-
-function Num(num1, num2)
-	if num1 > num2 then 
-		result = num1;
-	else 
-		result = num2; 
-	end
-	return result;
-end
-
-print("两值比较最大值为 ",Num(10,4))
-print("两值比较最大值为 ",Num(5,6))
-
-
-myprint = function(param)
-   print("这是打印函数 -   ##",param,"##")
-end
-
-function add(num1,num2,functionPrint)
-   result = num1 + num2
-   -- 调用传递的函数参数
-   functionPrint(result)
-end
-myprint(10)
--- myprint 函数作为参数传递
-add(2,0,myprint)
+--以只读（r）方式打开文件
+file = io.open("test.txt", 'r')
+--设置输入文件为 NewLuaFile.lua
+io.input(file)
+--输出
+print(io.read(-1))
+--关闭文件
+io.close(file)
